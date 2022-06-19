427f75b723f68b46e8ae2a58d2229915de3e16db
Mahdi Parivash <mahdiparivash@protonmail.com>
2022-06-19 21:22:20 +0430

    user route complate

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -20,18 +20,10 @@ export class UserService {
   }
 
   findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.UserRepository.find();
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.UserRepository.delete(id);
   }
 }
