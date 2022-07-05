550c68367195c9162ed7d601899a60619e63f0e9
Mahdi Parivash <mahdiparivash@protonmail.com>
2022-07-05 11:51:03 +0430

    add swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/guard/jwt.guard';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalGuards(new JwtAuthGuard());
+
+  const oprions = new DocumentBuilder()
+    .setTitle('TodoApp')
+    .setDescription('TodoApp API')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT Token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, oprions);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
