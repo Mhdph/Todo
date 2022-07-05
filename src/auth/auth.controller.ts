306f015e245c860e0c75a9a21e031d8290979f3b
Mahdi Parivash <mahdiparivash@protonmail.com>
2022-07-05 11:51:33 +0430

    create login dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 import { User } from 'src/user/entities/user.entity';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
+@ApiTags('Authenctication')
 export class AuthController {
   constructor(private jwtService: JwtService) {}
 
   @Post('/login')
   @UseGuards(AuthGuard('local'))
-  create(@Req() req) {
+  create(@Req() req, @Body() loginDto: LoginDto) {
     const user: User = req.user;
     const payload = {
       userId: user.id,
